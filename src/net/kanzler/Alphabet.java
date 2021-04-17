3ded45785dad8b8e419ed5b98e9a81002e2405d1
kanzler <nicholas.kanzler@gmial.com>
2021-04-18 02:33:01 +0300

    add Alphabet

---
+++ src/net/kanzler/Alphabet.java
@@ -0,0 +1,22 @@
+package net.kanzler;
+
+public class Alphabet {
+    public static void main(String[] args) {
+
+        char engCh = 65;            // Английская буква A
+        int engCount = 26;          // Кол-во букв в английском алфавите
+        printAlphabet(engCh, engCount);
+
+        char rusCh = 1040;          // Русская буква А
+        int rusCount = 33 - 1;      // Кол-во симовлов в русском алфавите - пропускаем Ё(1025)
+        printAlphabet(rusCh, rusCount);
+
+    }
+
+    private static void printAlphabet(int indexStart, int count) {
+        for (int i = indexStart; i < indexStart + count; i++) {
+            System.out.print((char) i + " ");
+        }
+        System.out.println();
+    }
+}
