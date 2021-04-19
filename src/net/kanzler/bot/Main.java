8f6dee975c082b404697b3cf7b62ad8f298cf583
nick <nicholas.kanzler@gmail.com>
2021-04-19 10:22:11 +0300

    add bot Kara

---
+++ src/net/kanzler/bot/Main.java
@@ -0,0 +1,58 @@
+package net.kanzler.bot;
+
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        greeting();
+        printYourName();
+        test();
+    }
+
+    private static void test() {
+        String name = "Nick".toUpperCase();
+        char[] nameChar = name.toCharArray();
+        for (char el : nameChar) {
+            if (el == 'N') {
+                System.out.println("*       *");
+                System.out.println("* *     *");
+                System.out.println("*   *   *");
+                System.out.println("*     * *");
+                System.out.println("*       *");
+            }
+            if (el == 'I') {
+                System.out.println("  *****  ");
+                System.out.println("    *    ");
+                System.out.println("    *    ");
+                System.out.println("    *    ");
+                System.out.println("  *****  ");
+            }
+            if (el == 'C') {
+                System.out.println("   ****  ");
+                System.out.println(" **      ");
+                System.out.println(" **      ");
+                System.out.println(" **      ");
+                System.out.println("   ****  ");
+            }
+            if (el == 'K') {
+                System.out.println("*   *    ");
+                System.out.println("*  *     ");
+                System.out.println("**       ");
+                System.out.println("*  *     ");
+                System.out.println("*    *   ");
+            }
+        }
+    }
+
+    private static void printYourName() {
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        System.out.printf("Hello, %s!\n", name);
+    }
+
+    private static void greeting() {
+        System.out.println("My name is Kara.");
+    }
+}
