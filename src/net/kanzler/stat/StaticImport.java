663661de5d4ba49e38a36bf22c3d77ebf0ef6444
nick <nicholas.kanzler@gmail.com>
2021-04-27 11:58:51 +0300

    add StaticImport

--- src/net/kanzler/stat/StaticImport.java
+++ src/net/kanzler/stat/StaticImport.java
@@ -1,6 +1,8 @@
 package net.kanzler.stat;
 
+
 import static java.lang.Math.*;
+import static java.util.Arrays.*;
 
 public class StaticImport {
     public static void main(String[] args) {
@@ -8,5 +10,9 @@ public class StaticImport {
 
         double x = sqrt(5) + pow(1,5) + abs(-5);
         System.out.println(x);
+
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.printf("The input string: %s\n", sc.nextLine());
     }
 }
